2140cdda737924acd2ce1f5e4db8d4280c924509
Kaan DOYURUR <kaan.doyurur@ens.uvsq.fr>
2022-02-06 14:26:11 +0100

    Exercice 01

---
+++ Ex01.py
@@ -0,0 +1,73 @@
+############################
+# Auteur : Kaan Doyurur
+############################
+# Import des librairies
+
+import tkinter as tk
+
+############################
+# Constantes
+
+HAUTEUR = 500
+LARGEUR = 500
+REC_LARGEUR = 400
+REC_HAUTEUR = 250
+
+############################
+# Variables
+
+compteur = 0
+############################
+# Fonctions
+
+
+def changer_couleur(event):
+    """Change la couleur du rectangle dans le canvas si et seulement si le clique souris à lieu sur le rectangle."""
+    global compteur
+    if event.x >= 0 and event.x <= REC_LARGEUR and event.y >= 0 and event.y <= REC_HAUTEUR:
+        couleur = ["red", "blue"]
+        compteur += 1
+        i = compteur % 2
+        canvas.itemconfigure(rectangle, fill=couleur[i])
+        print(compteur)
+    else:
+        canvas.unbind("<Button-1>")
+        compteur = 0
+        
+
+def recommencer():
+    """Applique la couleur rouge au rectangle et active le clique gauche de la souris"""
+    canvas.itemconfigure(rectangle, fill="red")
+    canvas.bind("<Button-1>", changer_couleur)
+
+
+########################################################
+# Partie principale
+
+############################
+#Création des widgets
+
+#Fenêtre
+root = tk.Tk()
+root.title("Exercice 01")
+content = tk.Frame(root) 
+
+#Canvas
+canvas = tk.Canvas(content, height=HAUTEUR, width=LARGEUR, bg="black")
+rectangle = canvas.create_rectangle(0, 0, REC_LARGEUR, REC_HAUTEUR, fill="red")
+
+#Bouton
+bouton_recommencer = tk.Button(content, text="Recommencer", command=recommencer)
+
+############################
+#Placement des widgets
+content.grid(column=0, row=0)
+canvas.grid(column=0, row=0)
+bouton_recommencer.grid(column=0, row=1)
+
+
+#Evenement
+canvas.bind("<Button-1>", changer_couleur)
+
+#Boucle principale
+root.mainloop()
